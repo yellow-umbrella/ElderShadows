152514eeb407b49e2b7391ff415c3a20c224a9bf
NSerytsan <nserytsan@gmail.com>
2023-11-20 20:46:05 +0200

    fix

--- Elder_Shadows/Assets/Scripts/Map/PlayerSpawn.cs
+++ Elder_Shadows/Assets/Scripts/Map/PlayerSpawn.cs
@@ -26,10 +26,14 @@ public class PlayerSpawn : MonoBehaviour
     public void MovePlayerOnGrass()
     {
         Vector3Int new_position = new Vector3Int(); 
-        string json = File.ReadAllText(Application.dataPath + "/homeLevel.json");
-        SpawnData data = JsonUtility.FromJson<SpawnData>(json);
 
-        AssertPosition = (int)(data.poses.Count / 1.75);
+        string json = File.ReadAllText(Application.dataPath + "/homeLevel.json");
+        string c_json = File.ReadAllText(Application.dataPath + "/homeCollisionsLevel.json");
+
+        SpawnData data = JsonUtility.FromJson<SpawnData>(json);
+        SpawnData c_data = JsonUtility.FromJson<SpawnData>(c_json);
+
+        AssertPosition = (int)((data.poses.Count + c_data.poses.Count / 2) / 2);
 
         if (data.tiles[AssertPosition] != null)
         {
@@ -38,6 +42,11 @@ public class PlayerSpawn : MonoBehaviour
 
         player.transform.position = new_position;
 
+    }
+
+    public void CheckPlayerPosition()
+    {
+        //Checks if originally player spawns on grass 
     }
 }
 public class SpawnData

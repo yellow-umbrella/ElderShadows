8634f467f2a4f0527265bdfb63778f50cd848527
VecroKatas <tymur.katas@gmail.com>
2024-02-15 15:27:17 +0200

    Item description close not functional

---
+++ Elder_Shadows/Assets/Scripts/UI/InventorySectionManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class InventorySectionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject descriptionBoxObject;
+
+    void Start()
+    {
+        Debug.Log(gameObject);
+        AddEvent(gameObject, EventTriggerType.PointerUp, delegate { OnUp(); });
+    }
+
+    private void AddEvent(GameObject obj, EventTriggerType type, UnityAction<BaseEventData> action)
+    {
+        EventTrigger trigger = obj.GetComponent<EventTrigger>();
+        var eventTrigger = new EventTrigger.Entry();
+        eventTrigger.eventID = type;
+        eventTrigger.callback.AddListener(action);
+        trigger.triggers.Add(eventTrigger);
+    }
+
+    private void OnUp()
+    {
+        if (descriptionBoxObject && descriptionBoxObject.activeSelf)
+            descriptionBoxObject.SetActive(false);
+    }
+}

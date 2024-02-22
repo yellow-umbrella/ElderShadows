4c41fa5bb4c89b3bb236b2c4e72641dc1004388e
VecroKatas <tymur.katas@gmail.com>
2024-02-22 21:09:58 +0200

    Equipped skills UI template

--- Elder_Shadows/Assets/Scripts/UI/SkillSubsectionManager.cs
+++ Elder_Shadows/Assets/Scripts/UI/SkillSubsectionManager.cs
@@ -4,7 +4,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SkillListManager : MonoBehaviour
+public class SkillSubsectionManager : MonoBehaviour
 {
     [SerializeField] private GameObject Skills;
     [SerializeField] private TextMeshProUGUI Icon;
